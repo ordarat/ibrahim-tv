0957a98e625d4bfed6c8168b02bdbe35b9d52a6f
ordarat <ordarat@users.noreply.github.com>
2026-05-11 11:17:56 +0000

    deploy: f4556d367b6251d177b270b616b0bafcf197ea4a

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3406765058" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2583465528" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
